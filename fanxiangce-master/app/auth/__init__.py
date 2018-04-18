603a53179358869525b53d28b4a49b5d6a57398e
huangjiusong <16444354@qq.com>
2018-04-18 23:38:15 +0800

    删除文件

--- fanxiangce-master/app/auth/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'lihui'
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-from . import views
--- fanxiangce-master/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*-coding: utf-8-*-
-from selenium import  webdriver
-from time import  sleep
-wd=webdriver.Chrome()
-wd.get('http://www.baidu.com')
-wd.close()
